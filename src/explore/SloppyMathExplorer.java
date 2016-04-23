ad559bceb1bc1f012829060109e09d10467b3213
amit <amit.kothiyal2004@gmail.com>
2016-04-23 21:13:06 +0200

    added explore package

---
+++ src/explore/SloppyMathExplorer.java
@@ -0,0 +1,12 @@
+package explore;
+
+import edu.stanford.nlp.math.*;
+
+public class SloppyMathExplorer {
+
+	public static void main(String[] args) {
+		double x = 10.123456789;
+		System.out.println(SloppyMath.round(x, 3));
+		System.out.println("hello world");
+	}
+}
